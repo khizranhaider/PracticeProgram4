6728004e698c3c1a1639d50ec5e3ad0c1bd9554f
khizranhaider <khizranhaider229422@gmail.com>
2021-07-23 20:56:59 +0500

    Initial commit

---
+++ src/ForLoop.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class ForLoop {
+    public static void main(String[] args) {
+        int n;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number : ");
+        n = sc.nextInt();
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(n + " X " + i + " = " + (n * i));
+        }
+    }
+}
